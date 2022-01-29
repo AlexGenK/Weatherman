008b6c4e8e62e095ccd0c0810829d9564432afc4
Oleksii Kozhhyn <suburbakskin@gmail.com>
2022-01-29 09:10:47 +0200

    Index of dimensions can be showed

--- app/controllers/dimensions_controller.rb
+++ app/controllers/dimensions_controller.rb
@@ -1,4 +1,15 @@
 class DimensionsController < ApplicationController
     def index
+        @dimensions = Dimension.order(:name)
     end
+    
+    private
+
+    def dimension_params
+        params.require(:station).permit(:name, :unit, :category, :guideline)
+    end
+
+    def set_dimension
+        @dimension = Dimension.find(params[:id])
+    end  
 end

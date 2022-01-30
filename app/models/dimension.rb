1291cdbf6ef5a35c8e5ef8cede05cc097509f5e3
Oleksii Kozhhyn <suburbakskin@gmail.com>
2022-01-30 13:50:00 +0200

    Correct declared method List of Dimension class

--- app/models/dimension.rb
+++ app/models/dimension.rb
@@ -2,6 +2,6 @@ class Dimension < ApplicationRecord
     has_many :measurements
 
     def self.list
-        Dimension.order(:name).collect {|p| [p.name, p.id]}
+        self.order(:name).collect {|p| [p.name, p.id]}
     end
 end

9f9bcd4d19bda9e29148deae6651e042c48cbc50
Oleksii Kozhhyn <suburbakskin@gmail.com>
2022-01-30 13:51:47 +0200

    Can be show weather of the selected station

--- app/controllers/weather_data_controller.rb
+++ app/controllers/weather_data_controller.rb
@@ -1,5 +1,11 @@
 class WeatherDataController < ApplicationController
     def index
-        @data = GetLastWeatherQuery.call('ecocity_083AF26E4F10', WEATHER_PARAMS)
+        @stations_list = Station.list
+        if params[:station_id]
+            @station = Station.find_by(influx_id: params[:station_id])
+        else
+            @station = Station.find_by(influx_id: @stations_list[0][1])
+        end
+        @data = GetLastWeatherQuery.call(@station.influx_id, WEATHER_PARAMS)
     end
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,7 @@
 class Station < ApplicationRecord
     has_many :measurements, dependent: :destroy
+
+    def self.list
+        self.order(:name).collect {|p| [p.name, p.influx_id]}
+    end
 end

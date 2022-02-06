81826d54e27987ab4afdebc7b947e942769e65ae
Oleksii Kozhhyn <suburbakskin@gmail.com>
2022-02-06 19:04:20 +0200

    Users can be deleted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_user, only: [:update, :destroy, :edit]
 
     def index
         @users = User.order(:email)
+    end
+
+    def destroy
+        if User.count <= 1
+          flash[:alert] = 'Невозможно удалить последнего пользователя'
+        else
+            @user.destroy
+        end
+        redirect_to users_path, status: 303
+    end
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])
     end
 end
